f1653910b57262d226891e55fa63f506f5e71519
Premasagar Rose <p@dharmafly.com>
2013-04-09 23:50:45 +0100

    Update styles

--- js/l4rp-events.js
+++ js/l4rp-events.js
@@ -3,6 +3,7 @@
     /////
 
     // ATOM DATE
+    // Source: https://github.com/premasagar/mishmash/tree/master/atomdate
 
     function atomdate(a){return typeof a=="string"?function(b){var c,d,e;return b=b.replace(/z$/i,"+00:00"),c=b.split(/[\-T:+]/),
     d=Number,c.length!==8?!1:(e=b.substr(19,1),new Date(Date.UTC(d(c[0]),d(c[1]-1),d(c[2]),d(c[3]-d(e+c[6])),d(c[4]-d(e+c[7])),
@@ -16,130 +17,161 @@
     // LANYRD
     
     function createSeriesWidget(){
-        var eventsWrapper = document.getElementById('events-wrapper'),
-            lanyrdWidget,
-            options,
-            loadingIndicator;
+        var noodleBaseUrl = 'http://dharmafly.noodle.jit.su/?q=',
+            eventsWrapper = document.getElementById('events-wrapper'),
+            lanyrdWidget, options, loadingIndicator, conferenceTemplate;
 
-        if (window.lanyrd && eventsWrapper) {
+        function addMetadata(html, conferences) {
+            // render the date icon for each event
+            $('.event-start-date').each(function (i, val) {
+                var datetime = $(this).attr('datetime');
+                dateIcon(datetime, this);
+            });
 
-            eventsWrapper.textContent = "Loading...";
+            // request the time of each event
+            var $timeElements = $('.lanyrd-series-time'),
+                times = [],
+                query, url;
 
-            loadingIndicator = loadingDots(eventsWrapper);
-
-            options = {
-                templates: {
-                    wrapper:'<div class="lanyrd-series">{{content}}</div>',
-
-                    upcoming:'<section class="lanyrd-series-upcoming"><h1>Upcoming Events...</h1>' +
-                    '<div>{{conferences}}</div></section>',
-
-                    upcomingConference:'<section id="event-{{slug}}" class="lanyrd-series-upcoming-conference callout">' + 
-                    '<time datetime="{{start_date}}" class="event-start-date">{{dates}}</time>' +
-                    '<h1><a href="{{web_url}}">{{name}}</a></h1>' +
-                    '<p class="lanyrd-series-time"></p>' +
-                    '<p>{{tagline}}</p><p><a href="{{web_url}}">&raquo; Read more & sign up on Lanyrd</a>' +
-                    '</p><ul class="lanyrd-series-topics"></ul>' +
-                    '<div class="lanyrd-series-attending"></div></section>',
-
-                    past:'<section class="lanyrd-series-past"><h1>Past Events...</h1>' +
-                    '<div>{{conferences}}</div></section>',
-
-                    /*
-                    pastConference:'<section class="lanyrd-series-past-conference callout">' +
-                    '<time datetime="{{start_date}}" class="event-start-date">{{dates}}</time>' +
-                    '<h1><a title="{{tagline}}" href="{{web_url}}">{{name}}</a></h1>' +
-                    //'<p><a class="series-past-more-info" href="{{web_url}}">&raquo; More Info</a></p>' +
-                    '<div class="lanyrd-series-more-info"></div></section>',
-                    */
-
-                    pastConference: '<section id="event-{{slug}}" class="lanyrd-series-past-conference callout">' + 
-                    '<time datetime="{{start_date}}" class="event-start-date">{{dates}}</time>' +
-                    '<h1><a href="{{web_url}}">{{name}}</a></h1>' +
-                    '<p class="lanyrd-series-time"></p>' +
-                    '<p>{{tagline}}</p><p><a href="{{web_url}}">&raquo; Read more on Lanyrd</a>' +
-                    '</p><ul class="lanyrd-series-topics"></ul>' +
-                    '<div class="lanyrd-series-attending"></div></section>',
-
-                    error:'Oops. We couldn\'t load the details just now.'
-                }
-            };
-
-            lanyrdWidget = lanyrd.widgets.series('http://lanyrd.com/series/l4rp/', eventsWrapper, options);
-
-            lanyrdWidget.always(function(){
-
-                loadingIndicator.stop();
-
-            }).done(function (html, conferences) {
-
-                // render the date icon for each event
-                $('.event-start-date').each(function (i, val) {
-                    var datetime = $(this).attr('datetime');
-                    dateIcon(datetime, this);
-                });
-
-                // request the time of each event
-                var $timeElements = $('.lanyrd-series-time'),
-                    times = [], 
-                    url;
-
-                $timeElements.each(function (i, val) {
+            $timeElements.each(function (i, val) {
+                var url = $(this).parent().find('h1 a').attr('href');
+                if (url){
                     times.push({
-                        url: $(this).siblings('h1').find('a').attr('href'),
+                        url: url,
                         selector: '.dtstart .time',
                         extract: 'text'
                     });
-                });
+                }
+            });
 
-                times = encodeURIComponent(JSON.stringify(times));
-                url = 'http://dharmafly.noodle.jit.su/?q=' + times + '&callback=?';
+            query = encodeURIComponent(JSON.stringify(times));
+            url = noodleBaseUrl + query + '&callback=?';
 
-                $.getJSON(url, function (data) {
+            $.getJSON(url, function (data) {
+                if (data){
                     $timeElements.each(function (i, val) {
-                        if (data[i].results.length > 0) {
+                        if (data[i].results && data[i].results.length) {
                             $(this).text(data[i].results[0].text);
                         }
                     });
-                });
+                }
+            });
 
-                // build and render the topics for each conference
-                $('.lanyrd-series-topics').each(function (i, val) {
-                    var conference,
-                        $list = $(this),
-                        href = $list.siblings('h1').find('a').attr('href');
+            // build and render the topics for each conference
+            $('.lanyrd-series-topics').each(function (i, val) {
+                var $list = $(this),
+                    url = $list.parent().find('h1 a').eq(0).attr('href'),
+                    topicsHtml = '',
+                    conference, i, len;
 
-                    // get the conference object for this topics list
-                    for (var i = 0; i < conferences.length; i++) {
-                        if (conferences[i].conference.web_url === href) {
-                            conference = conferences[i].conference;
-                            break;
+                // get the conference object for this topics list
+                for (i=0, len = conferences.length; i < len; i++) {
+                    if (conferences[i].conference.web_url === url) {
+                        conference = conferences[i].conference;
+
+                        if (conference.topics){
+                            $.each(conference.topics, function (i, val) {
+                                topicsHtml += '<li><a href="' + val.web_url + '">' + val.name + '</a></li>';
+                            });
                         }
+                    }
+                }
+                if (topicsHtml){
+                    $list.append(topicsHtml);
+                }
+            });
+
+            // Get event description
+            var $descElements = $('.description'),
+                descs = [],
+                query, url;
+
+            $descElements.each(function (i, val) {
+                var url = $(this).parent().find('h1 a').attr('href');
+                if (url){
+                    descs.push({
+                        url: url,
+                        selector: '#event-description',
+                        extract: 'html'
+                    });
+                }
+            });
+
+            query = encodeURIComponent(JSON.stringify(descs));
+            url = noodleBaseUrl + query + '&callback=?';
+
+            $.getJSON(url, function (data) {
+                if (data){
+                    $descElements.each(function (i, val) {
+                        if (data[i].results && data[i].results.length) {
+                            $(this).append(data[i].results[0].html);
+                        }
+                    });
+                }
+            });
+
+
+            // Load the people widgets
+            $('.lanyrd-series-attending').each(function (i, val) {
+                var $link   = $(this).siblings('p').children('a'),
+                    href    = $link.attr('href'),
+                    dots    = loadingDots($link[0], {reset:true,interval:600}),
+                    options = {
+                        append: true,
+                        attendeesHeadingTemplate: '<h2 class="lanyrd-attendees-title">{{amount}} attending</h2>',
+                        trackersHeadingTemplate: '<h2 class="lanyrd-trackers-title">{{amount}} tracking</h2>',
                     };
 
-                    // render the topics list
-                    $.each(conference.topics, function (i, val) {
-                        $list.append('<li><a href="' + val.web_url + '">' + val.name + '</a></li>');
-                    });
-                });
-
-                // Load the people widgets
-                $('.lanyrd-series-attending').each(function (i, val) {
-                    var $link   = $(this).siblings('p').children('a'),
-                        href    = $link.attr('href'),
-                        dots    = loadingDots($link[0], {reset:true,interval:600}),
-                        options = {
-                            append: true,
-                            attendeesHeadingTemplate: '<h2 class="lanyrd-attendees-title">{{amount}} attending</h2>',
-                            trackersHeadingTemplate: '<h2 class="lanyrd-trackers-title">{{amount}} tracking</h2>',
-                        };
-
-                    lanyrd.widgets.people(href, this, options).done(function () {
-                        dots.stop();
-                    });
+                lanyrd.widgets.people(href, this, options).done(function () {
+                    dots.stop();
                 });
             });
         }
+
+        if (!window.lanyrd || !eventsWrapper) {
+            return;
+        }
+
+        eventsWrapper.textContent = "Loading...";
+
+        loadingIndicator = loadingDots(eventsWrapper);
+
+        conferenceTemplate = '<header>' +
+                '<time datetime="{{start_date}}" class="event-start-date">{{dates}}</time>' +
+                '<h1><a href="{{web_url}}">{{name}}</a></h1>' +
+            '</header>' +
+            '<p class="lanyrd-series-time"></p>' +
+            '<p class="summary">{{tagline}}</p>' +
+            '<p><a class="track" href="{{web_url}}">Read more / sign up</a></p>' +
+            '<section class="description"></section>' +
+            '<ul class="lanyrd-series-topics"></ul>' +
+            '<div class="lanyrd-series-attending"></div>';
+
+        options = {
+            templates: {
+                wrapper: '<div class="lanyrd-series">{{content}}</div>',
+
+                upcoming: '<section class="lanyrd-series-upcoming">' +
+                    '<h1 class="series">Upcoming Events...</h1>' +
+                    '<div>{{conferences}}</div>' + 
+                '</section>',
+
+                upcomingConference: '<section id="event-{{slug}}" class="event lanyrd-series-upcoming-conference">' + conferenceTemplate + '</section>',
+
+                past: '<section class="lanyrd-series-past"><h1 class="series">Past Events...</h1>' +
+                '<div>{{conferences}}</div></section>',
+
+                pastConference: '<section id="event-{{slug}}" class="event lanyrd-series-past-conference">' + conferenceTemplate + '</section>',
+
+                error:'Oops. We couldn\'t load the details just now.'
+            }
+        };
+
+        lanyrdWidget = lanyrd.widgets.series('http://lanyrd.com/series/l4rp/', eventsWrapper, options);
+
+        lanyrdWidget.always(function(){
+            loadingIndicator.stop();
+        }).done(addMetadata);
     }
 
     function dateIcon(datetime, element) {
